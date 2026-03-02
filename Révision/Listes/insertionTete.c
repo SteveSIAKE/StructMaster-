38ef4f7908bd882554f07423f542ec83ee355c25
SteveSIAKE <siakesteve4@gmail.com>
2026-03-02 03:00:48 +0100

    chore: initialisation de l'architecture StructMaster et documentation

--- Révision/Listes/insertionTete.c
+++
@@ -1,5 +0,0 @@
-Node* insertionTete(Node* head, int valeur) {
-    Node* n = creerNoeud(valeur);
-    n->next = head;
-    return n;
-}
--- Révision/Listes/tailleListe.c
+++
@@ -1,8 +0,0 @@
-int tailleListe(Node* head) {
-    int count = 0;
-    while (head != NULL) {
-        count++;
-        head = head->next;
-    }
-    return count;
-}
